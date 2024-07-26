eddbc01a0aadabf4ef879469009d66b596e66391
NyaxDa <nyaxda@gmail.com>
2024-07-25 18:53:07 -0700

    Errorfix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,21 +2,21 @@
 """
 Pascals Triangle
 """
-import math
-
-
-def binomial(n, k):
-    """binomial"""
-    return math.comb(n, k)
 
 
 def pascal_triangle(n):
-    """pascal_triangle"""
-    if n <= 0:
-        return []
-
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
     triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
     for i in range(n):
-        row = [binomial(i, j) for j in range(i + 1)]
-        triangle.append(row)
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
     return triangle
