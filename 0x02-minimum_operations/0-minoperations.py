becb63324c56404ef603ad3dcfa4e53686735b40
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-09 04:21:23 +0300

    Added File 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Minimum Operations module"""
+
+
+def minOperations(n):
+    """minimum operations to be done to result to exatcly n xters in file"""
+    count = 0
+    prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    while (n != 1):
+        for i in prime_numbers:
+            if n % i == 0:
+                n = n // i
+                count += i
+                break
+    return count
