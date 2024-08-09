cfb1fd9c06348f012e51710512b90700c226e333
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-09 04:29:18 +0300

    chore: Refactor minOperations function to handle edge cases

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,14 +4,14 @@
 
 def minOperations(n):
     """minimum operations to be done to result to exatcly n xters in file"""
-    if (n == 0 or n == 1):
+    if (n <= 1):
         return 0
     count = 0
-    prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
-    while (n != 1):
-        for i in prime_numbers:
+    factor = 2
+    while (n > 1):
+        while n % factor == 0:
+            count += factor
             if n % i == 0:
-                n = n // i
-                count += i
-                break
+                n //= factor
+                factor += 1
     return count
